3cb196052a93ff7b3b6b0bc6d1a55fce83b225b3
alamalcantara <alamalcantara99@gmail.com>
2021-06-27 14:25:43 -0400

    feat: enabling cors

--- src/main/java/com/example/students/StudentsApiApplication.java
+++ src/main/java/com/example/students/StudentsApiApplication.java
@@ -1,37 +1,15 @@
 package com.example.students;
 
-import java.util.Date;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.students.entities.Student;
-import com.example.students.services.StudentService;
 
 @SpringBootApplication
-public class StudentsApiApplication implements ApplicationRunner {
-
-	@Autowired
-	private StudentService studentService;
-	
-	
-	private static final Logger log = LoggerFactory.getLogger(StudentsApiApplication.class);
+public class StudentsApiApplication {
 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsApiApplication.class, args);
-	}
-
-	//TODO: remove when the database is ready
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		// TODO Auto-generated method stub
-
 	}
 
 }
